0ac45d20ad01f914132eea3883297b87e5f093d5
Ξunit <eunithelp@gmail.com>
2022-09-22 00:17:53 +0100

    Add example code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-green-200 rounded-xl shadow">
+      <div className="text-3xl text-green-700 font-bold mb-5">
+         Welcome!
+      </div>
+      <p className="text-green-500 text-lg">
+        I am learning Tailwind CSS
+      </p>
     </div>
   );
 }
