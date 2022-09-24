9d1dda8767ed4f39f327f8bcded5c13ebf85803b
Ξunit <eunithelp@gmail.com>
2022-09-24 08:05:23 +0100

    Feat: Update comtent

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@
 
 function App() {
   return (
-    <div className="container mx-auto bg-green-200 rounded-xl shadow">
-      <div className="text-3xl text-green-700 font-bold mb-5">
-         Welcome!
+    <div className="container w-80 mt-5 mx-auto bg-green-200 rounded-xl shadow">
+      <div className="text-3xl p-8 text-green-700 font-bold mb-5">
+        Welcome!
+        <p className="text-green-500 mt-3 text-lg">
+          I am learning Tailwind CSS
+        </p>
       </div>
-      <p className="text-green-500 text-lg">
-        I am learning Tailwind CSS
-      </p>
     </div>
   );
 }
